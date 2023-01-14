e2e477a3e40bec90b854f28eea261bc27e44a5b2
mozzi327 <mozzi327@gmail.com>
2023-01-14 20:51:12 +0900

    feat: section2 Spring Cloud 유레카 로드 밸런싱

--- first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,15 +1,21 @@
 package com.example.firstservice.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    private final Environment environment; // yml 설정 파일 가져오는 클래스
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +29,9 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest req) {
+        log.info("Server Port={}", req.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s",
+                environment.getProperty("local.server.port"));
     }
 }
